a44ef03100350cd5ceedf99de31ceddc24f202b3
Cesar Koprowski <173203133+cesarkoprowski@users.noreply.github.com>
2024-07-25 17:29:42 -0300

    Increasing the solution to exercise 1043

---
+++ exercises/1043-triangles/triangles.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void verify_triangle(float a, float b, float c);
+void perimeter(float a, float b, float c);
+void area_of_trapezium(float a, float b, float c);
+
+int main() {
+    float a, b, c;
+    cin >> a >> b >> c;
+    verify_triangle(a, b, c);
+    return 0;
+}
+
+void verify_triangle(float a, float b, float c) {
+    int aux;
+    if (((b + c) > a) && (b - c) < a) {
+        perimeter(a, b, c);
+    } else {
+        area_of_trapezium(a, b, c);
+    }
+}
+
+void perimeter(float a, float b, float c) {
+    cout.precision(1);
+    cout << "Perimetro = " << fixed << a + b + c << endl;
+}
+
+void area_of_trapezium(float a, float b, float c) {
+    cout.precision(1);
+    cout << "Area = " << fixed << ((a+b)*c)/2 << endl;
+}
