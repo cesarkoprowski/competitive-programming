27227016ce701eefde97b1bdb6f95382264f05a4
Cesar Koprowski <173203133+cesarkoprowski@users.noreply.github.com>
2024-09-16 21:26:40 -0300

    Add solution for hackerrank numpy exercise 3.

--- hackerrank/numpy/3-mean-var-and-std.py
+++ hackerrank/numpy/3-mean-var-and-std.py
@@ -8,6 +8,10 @@ for i in range(n):
     linha = list(map(int, input().split()))
     v[i, :] = linha  
 
-print(np.mean(v, axis = 1))
-print(np.var(v, axis = 0))
-print(np.std(v))
+print(np.mean(v, axis=1)) 
+print(np.var(v, axis=0))  
+result = np.std(v, axis=None)
+if result != 0:
+    print(f"{result:.11f}") 
+else:
+    print(f"{result:.1f}") 
