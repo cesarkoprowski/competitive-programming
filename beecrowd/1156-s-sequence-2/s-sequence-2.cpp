90897226ab3256ea9f7946f8d1a9578be8eb8474
Cesar Koprowski <173203133+cesarkoprowski@users.noreply.github.com>
2024-10-25 13:41:51 -0300

    Add solution to beecrowd exercises 1155 and 1156

--- beecrowd/1156-s-sequence-2/s-sequence-2.cpp
+++ beecrowd/1156-s-sequence-2/s-sequence-2.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main() {
     double s = 1;
-    for (int i = 2; i <= 100; i++) {
-        s += 1.0/i;
+    int i, j, k;
+    for (i = 1, j = 3, k = 2; j < 40; i++, j += 2, k *= 2) {
+        s += j*1.0/k;
     }
     printf("%.2lf\n", s);
     return 0;
